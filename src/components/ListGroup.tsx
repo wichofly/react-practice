a9a5df37f4e01547ed3c040415a0013fd3cdc5f6
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-27 16:41:22 +0100

    feat: use of braces { } inside the JSX to work.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
-const cities = [
+let cities = [
   'San Salvador',
   'San Miguel',
   'Santa Ana',
   'San Antonio',
   'San Francisco'
 ]
+cities = []
 
 const ListGroup = () => {
   return (
     <>
       <h1>🕺 Cities 🏄‍♂️</h1>
+      {cities.length === 0 && <p>No Cities</p>}
       <ul className="list-group">
         {cities.map(city => <li key={city}>
           {city}
