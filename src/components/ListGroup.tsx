266258c764fcf44aafbf6a78f469652dc90bb394
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-27 18:08:23 +0100

    feat: use of MouseEvent, one of the good thing of TypeScript

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react"
+
 let cities = [
   'San Salvador',
   'San Miguel',
@@ -5,23 +7,17 @@ let cities = [
   'San Antonio',
   'San Francisco'
 ]
-cities = []
 
 const ListGroup = () => {
 
-  // const getPlaces = () => {
-  //   return cities.length === 0 && <p>No Cities</p>
-  // }
-
-  const getPlaces = () => cities.length === 0 && <p>No Cities</p> 
-
+  const handleClick = (evt: MouseEvent) => console.log(evt.target)
 
   return (
     <>
       <h1>🕺 Cities 🏄‍♂️</h1>
-      {getPlaces()}
+      {cities.length === 0 && <p>No Cities found</p>}
       <ul className="list-group">
-        {cities.map(city => <li key={city}>
+        {cities.map(city => <li key={city} className="list-group-item" onClick={handleClick}>
           {city}
         </li>)}
       </ul>
@@ -30,3 +26,18 @@ const ListGroup = () => {
 }
 
 export default ListGroup
+
+/*
+  - MouseEvent 
+    Helps use to use all the option we have to use the handleClick.
+    Outside of the JSX, using the arrow function, the evt has an error, it can not recognize the type of the parameter. is it a number, string or boolean?.
+    MouseEvent helps to show the properties using Type annotation in TypeScript. This option is one of the beauties of TypeScript.
+      - We get Auto-Completion
+      - We get type safety
+      - it is easier to refactor or restructure our code 
+      
+  ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+*/
