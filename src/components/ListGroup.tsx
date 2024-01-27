aa105c33873b4d5261ef7185608d6ae506bab031
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-27 17:08:08 +0100

    feat: make a function to render the cities.
    We can see the function using braces with return and without braces

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,10 +8,18 @@ let cities = [
 cities = []
 
 const ListGroup = () => {
+
+  // const getPlaces = () => {
+  //   return cities.length === 0 && <p>No Cities</p>
+  // }
+
+  const getPlaces = () => cities.length === 0 && <p>No Cities</p> 
+
+
   return (
     <>
       <h1>🕺 Cities 🏄‍♂️</h1>
-      {cities.length === 0 && <p>No Cities</p>}
+      {getPlaces()}
       <ul className="list-group">
         {cities.map(city => <li key={city}>
           {city}
