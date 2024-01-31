a56b08edc3bcc9a80662c3095bebf22e908f0edc
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-31 20:10:35 +0100

    feat: Explanation of the proper use of State.

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,36 @@ import { useState } from 'react'
 import { v4 as uuid } from 'uuid'
 
 import './App.css'
+import ExtendableText from './components/4-Managing-Component-State/ExtendableText'
 
 
 function App() {
-  const [game, setGame] = useState({
-    id: uuid(),
-    player: {
-      name: 'Tobias'
-    }
-  })
+  // const [game, setGame] = useState({
+  //   id: uuid(),
+  //   player: {
+  //     name: 'Tobias'
+  //   }
+  // })
 
-  const handleClick = () => {
-    setGame({
-      ...game,
-      player: { ...game.player, name: 'Moises' }
-    })
-  }
+  // const handleClick = () => {
+  //   setGame({
+  //     ...game,
+  //     player: { ...game.player, name: 'Moises' }
+  //   })
+  // }
 
   return (
     <div className="App">
-      <button onClick={handleClick}>New Gamer</button>
+      <ExtendableText >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam distinctio id nulla sint,
+        nemo quos? Quo animi, laudantium similique necessitatibus fugit pariatur aut ea! Quod eligendi, obcaecati, quo assumenda ex provident impedit totam soluta corrupti quia repellendus? Officia tenetur molestiae,
+        quam voluptatem a quibusdam non maiores fugit aliquid nesciunt aliquam eum odit blanditiis aut atque sapiente, nobis nihil porro voluptatibus dolores. Aliquid minima tenetur, laudantium saepe laboriosam illum facilis modi accusantium,
+        maxime iure adipisci vel sed quam eligendi tempore similique ducimus odio? Soluta ipsam fuga molestiae voluptatibus velit quae possimus at aliquid, voluptates consectetur vel tempore veniam voluptatum debitis provident.
+      </ExtendableText>
+
+      <ExtendableText >
+        Hello Chasse 
+      </ExtendableText>
     </div >
   )
 }
