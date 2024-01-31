91749574d7dfe2320f378bcb8ba57bc457536548
Mauricio Erazo <antonierazo13@hotmail.com>
2024-01-31 20:45:13 +0100

    feat: building normal forms
    useful website:
    https://www.react-hook-form.com/get-started/

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid'
 
 import './App.css'
 import ExtendableText from './components/4-Managing-Component-State/ExtendableText'
+import Form from './components/5-Building-Forms/Form'
 
 
 function App() {
@@ -22,16 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <ExtendableText >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam distinctio id nulla sint,
-        nemo quos? Quo animi, laudantium similique necessitatibus fugit pariatur aut ea! Quod eligendi, obcaecati, quo assumenda ex provident impedit totam soluta corrupti quia repellendus? Officia tenetur molestiae,
-        quam voluptatem a quibusdam non maiores fugit aliquid nesciunt aliquam eum odit blanditiis aut atque sapiente, nobis nihil porro voluptatibus dolores. Aliquid minima tenetur, laudantium saepe laboriosam illum facilis modi accusantium,
-        maxime iure adipisci vel sed quam eligendi tempore similique ducimus odio? Soluta ipsam fuga molestiae voluptatibus velit quae possimus at aliquid, voluptates consectetur vel tempore veniam voluptatum debitis provident.
-      </ExtendableText>
-
-      <ExtendableText >
-        Hello Chasse 
-      </ExtendableText>
+      <Form />
     </div >
   )
 }
