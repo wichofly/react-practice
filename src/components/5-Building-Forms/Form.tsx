ab3b918e2f73f56961c4a47bb442055819902962
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-01 17:35:34 +0100

    feat: disable the submit button if the form is invalid.
    Using the disable does not show the messages: '3   Characters at least!', 'Age field is required.', 'Age must be at least 18'

--- src/components/5-Building-Forms/Form.tsx
+++ src/components/5-Building-Forms/Form.tsx
@@ -22,7 +22,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors, isValid }
   } = useForm<FormData>({ resolver: zodResolver(schema) })
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -51,7 +51,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger" >{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button disabled={!isValid} className="btn btn-primary" type="submit">Submit</button>
     </form>
   )
 }
