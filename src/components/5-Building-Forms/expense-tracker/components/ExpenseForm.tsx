e3e06fdeae6498bea8e329865c2b466320f2be51
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-01 23:43:27 +0100

    feat: add messages to show the user the nice way to use categories

--- src/components/5-Building-Forms/expense-tracker/components/ExpenseForm.tsx
+++ src/components/5-Building-Forms/expense-tracker/components/ExpenseForm.tsx
@@ -5,12 +5,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import categories from '../categories'
 
 const schema = z.object({
-  description: z.string().min(5).max(100),
+  description: z.string().min(5, { message: 'Description should be at least 5 characters' }).max(100),
   amount: z
     .number({ invalid_type_error: 'Amount field is required.' })
     .min(0.01)
     .max(100_000), // maximum of $100,000
-  category: z.enum(categories)
+  category: z.enum(categories, {
+    errorMap: () => ({ message: 'Category is required' })
+  })
 })
 
 type ExpenseFormData = z.infer<typeof schema>
@@ -23,7 +25,7 @@ const ExpenseForm = () => {
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) })
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(data => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
@@ -42,7 +44,7 @@ const ExpenseForm = () => {
           Amount
         </label>
         <input
-          {...register('amount')}
+          {...register('amount', { valueAsNumber: true })}
           id="amount"
           type="number"
           className="form-control"
@@ -68,7 +70,7 @@ const ExpenseForm = () => {
         {errors.category && <p className='text-danger'>{errors.category.message}</p>}
       </div>
       <button className="btn btn-primary">Submit</button>
-    </form>
+    </form >
   )
 }
 
