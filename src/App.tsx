afd7a025c0537cfeeed7a6e9acd607466fa763e1
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-09 23:44:22 +0100

    feat: async and away used.
    * A function is needed inside useEffect to use async and await.
       It allows you to write code that looks like it's running sequentially, even though it's actually running asynchronously.
    
     * "catch" is used in different way using AxiosError:
        catch (err) {
          setError((err as AxiosError).message)
        }

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 import ProductList from './components/6-Connecting-Backend/ProductList'
 
@@ -17,9 +17,20 @@ function App() {
 
   // After only first render
   useEffect(() => {
-    axios(apiUsers)
-      .then(res => setUsers(res.data))
-      .catch(err => setError(err.message))
+    const fetchDataUsers = async () => {
+      try {
+        const res = await axios(apiUsers)
+        setUsers(res.data)
+      } catch (err) {
+        setError((err as AxiosError).message)
+      }
+    }
+
+    fetchDataUsers()
+
+    // axios(apiUsers)
+    //   .then(res => setUsers(res.data))
+    //   .catch(err => setError(err.message))
   }, [])
 
   return (
@@ -32,7 +43,7 @@ function App() {
       <ProductList category={category} /> */}
 
       {error && <p className='text-danger'>{error}</p>}
-      
+
       <ul>
         {users.map(user =>
           <li key={user.id}>{user.name}</li>
@@ -44,4 +55,13 @@ function App() {
 
 export default App
 
+/*
+ * A function is needed inside useEffect to use async and await.
+   It allows you to write code that looks like it's running sequentially, even though it's actually running asynchronously. 
 
+ * "catch" is used in different way using AxiosError:
+    catch (err) {
+      setError((err as AxiosError).message)
+    }
+
+*/
