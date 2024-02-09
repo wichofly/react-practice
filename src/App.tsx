804564fb6e9c35f5c7767b1701540a309d83737b
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-09 22:30:00 +0100

    feat: fetching data
    getting name from users

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import ProductList from './components/6-Connecting-Backend/ProductList'
 
-function App() {
-  const [category, setCategory] = useState('')
+interface User {
+  id: number
+  name: string
+}
 
-  // After each render
-  // useEffect(() => {
-  // }, [])
+function App() {
+  const [users, setUsers] = useState<User[]>([])
+  // const [category, setCategory] = useState('')
+
+  const apiUsers = 'https://jsonplaceholder.typicode.com/users'
+
+  // After only first render
+  useEffect(() => {
+    axios(apiUsers)
+      .then(res => setUsers(res.data))
+  }, [])
 
   return (
     <div >
-      <select className="form-select" onChange={(evt) => setCategory(evt.target.value)}>
+      {/* <select className="form-select" onChange={(evt) => setCategory(evt.target.value)}>
         <option value=""></option>
         <option value="Clothing">Clothing</option>
         <option value="Household">Household</option>
       </select>
-      <ProductList category={category} />
+      <ProductList category={category} /> */}
+
+      <ul>
+        {users.map(user =>
+          <li key={user.id}>{user.name}</li>
+        )}
+      </ul>
     </div >
   )
 }
